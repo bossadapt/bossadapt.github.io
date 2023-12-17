1307f554512f78718002fcd9c7e7129cbd5a669d
bossadapt <37967493+bossadapt@users.noreply.github.com>
2023-12-17 14:07:04 -0600

    fixing mobile

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -2,10 +2,10 @@ export const Contact: React.FC = () => {
   return (
     <div className="contacts">
       <hr></hr>
-      <p style={{ fontSize: "30px", marginLeft: "20px", marginRight: "20px" }}>
+      <p style={{ marginLeft: "20px", marginRight: "20px" }}>
         Email: Devinphillips20@gmail.com
       </p>
-      <p style={{ fontSize: "30px", marginLeft: "20px", marginRight: "20px" }}>
+      <p style={{ marginLeft: "20px", marginRight: "20px" }}>
         Linkedin:{" "}
         <a href="www.linkedin.com/in/devin-phillips-040699191">
           Devin Phillips

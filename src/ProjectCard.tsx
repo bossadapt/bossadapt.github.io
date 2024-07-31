d717602ef285a1b9f05c836be7d39b01330b6c3e
bossadapt <37967493+bossadapt@users.noreply.github.com>
2024-07-31 01:53:27 -0500

    targeting blank tabs and adding images for socials

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -1,15 +1,15 @@
-import HnS from "./assets/HnS.png";
-import ImporovedStartup from "./assets/ImporovedStartup.png";
-import LeagueVersus from "./assets/LeagueVersus.png";
-import TalkItOut from "./assets/TalkItOut.png";
-import TFTRolls from "./assets/TFTRolls.png";
-import VideoHijack from "./assets/VideoHijack.png";
-import LeagueRolls from "./assets/LeagueRolls.png";
-import SteamRecommendation from "./assets/SteamRecommendation.png";
-import TheLastHope from "./assets/TheLastHope.png";
-import BasicRolls from "./assets/BasicRolls.png";
-import SteamCrawler from "./assets/SteamCrawler.png";
-import SteamRecAI from "./assets/SteamRecAI.png";
+import HnS from "./assets/project/HnS.png";
+import ImporovedStartup from "./assets/project/ImporovedStartup.png";
+import LeagueVersus from "./assets/project/LeagueVersus.png";
+import TalkItOut from "./assets/project/TalkItOut.png";
+import TFTRolls from "./assets/project/TFTRolls.png";
+import VideoHijack from "./assets/project/VideoHijack.png";
+import LeagueRolls from "./assets/project/LeagueRolls.png";
+import SteamRecommendation from "./assets/project/SteamRecommendation.png";
+import TheLastHope from "./assets/project/TheLastHope.png";
+import BasicRolls from "./assets/project/BasicRolls.png";
+import SteamCrawler from "./assets/project/SteamCrawler.png";
+import SteamRecAI from "./assets/project/SteamRecAI.png";
 const imageList = new Map([
   ["SteamRec AI", SteamRecAI],
   ["Steam Crawler", SteamCrawler],
@@ -35,7 +35,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
   githubURL,
   tags,
 }) => (
-  <a href={githubURL} className="no-style">
+  <a href={githubURL} className="no-style" target="_blank">
     <div
       className="project-card"
       style={{
--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import linkdin from "./assets/welcome/linkedin.png";
+import github from "./assets/welcome/github.png";
+import gmail from "./assets/welcome/gmail.png";
 export const Welcome: React.FC = () => {
   const [styles, setStyles] = useState({
     to: { width: "0%" },
@@ -40,14 +43,33 @@ export const Welcome: React.FC = () => {
           style={{ fontSize: "30px", marginLeft: "20px", marginRight: "20px" }}
         >
           Thanks for visiting my tiny corner of the web. My name is Devin
-          Phillips, I enjoy coding and videogames. You can find me on{" "}
-          <a href="https://github.com/bossadapt">Github</a>,{" "}
-          <a href="https://www.linkedin.com/in/devin-phillips-040699191/">
-            Linkedin
-          </a>{" "}
-          or reach me via email at devinphillips20@gmail.com. Please enjoy your
-          stay.
+          Phillips, I enjoy coding and videogames. Please enjoy your stay.
         </p>
+        <div className="socialContainer">
+          <a
+            className="socialWrapper"
+            href="https://www.linkedin.com/in/devin-phillips-040699191"
+            target="_blank"
+          >
+            <img src={linkdin} className="social"></img>
+          </a>
+          <hr />
+          <a
+            className="socialWrapper"
+            href="https://github.com/bossadapt"
+            target="_blank"
+          >
+            <img src={github} className="social"></img>
+          </a>
+          <hr />
+          <a
+            className="socialWrapper"
+            href="mailto: devinphillips20@gmail.com"
+            target="_blank"
+          >
+            <img src={gmail} className="social"></img>
+          </a>
+        </div>
       </div>
     </div>
   );
